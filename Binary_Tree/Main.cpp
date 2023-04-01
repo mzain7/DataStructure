20e859d5e2cf59c81347c030b59850f7453a27f4
Zain-03 <mzain.akhtar3@gmail.com>
2023-04-01 19:40:08 +0500

    updated

--- Binary_Tree/Main.cpp
+++ Binary_Tree/Main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char const *argv[])
     BinaryTree<int> tree;
     tree.insert(4);
     tree.insert(24);
-    tree.insert(25);
+    tree.insert(26);
     tree.insert(9);
     tree.insert(0);
     tree.insert(2);
@@ -21,16 +21,19 @@ int main(int argc, char const *argv[])
     tree.insert(4);
     tree.insert(4);
     tree.insert(4);
-    tree.insert(4);
+    tree.insert(25);
 
     std::regex r ("[, ]+");
-    std::cout << std::regex_replace(tree.inorderToString(tree.root),r,", ");
-    std::cout << tree.search(4)->data;
-    tree.deleteRoot();
+    // std::cout << std::regex_replace(tree.inorderToString(tree.root),r,", ");
+    // std::cout << tree.search(4)->data;
+    // tree.deleteRoot();
     std::cout <<std::endl<< std::regex_replace(tree.inorderToString(tree.root),r,", ");
-    tree.deleteNode(5);
+    std::cout << tree.successor(tree.search(24))[0]->data << " and " << tree.successor(tree.search(24))[1]->data;
+    tree.deleteNode(24);
     std::cout <<std::endl<< std::regex_replace(tree.inorderToString(tree.root),r,", ");
     // tree.rearrange();
+
+    // tree.bfs_iterative();
 
     return 0;
 }
