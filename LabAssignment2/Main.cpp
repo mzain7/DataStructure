e9f2bc94c1934070a75a637b870be1d1273d78d4
Zain-03 <mzain.akhtar3@gmail.com>
2023-04-06 22:58:12 +0500

    updated

--- LabAssignment2/Main.cpp
+++ LabAssignment2/Main.cpp
@@ -26,6 +26,41 @@ int main(int argc, char const *argv[])
     std::cout<<bought.front().price<<std::endl;
     std::cout<<ctime(&bought.front().time)<<std::endl;
 
+
+    // Transaction trans;
+    trans.count = 10;
+    trans.price = 30;
+    trans.time = time(0);
+
+    sold.push(trans);
+    std::cout<<sold.front().count<<std::endl;
+    std::cout<<sold.front().price<<std::endl;
+    std::cout<<ctime(&sold.front().time)<<std::endl;
+
+    Transaction buy, sell;
+    while(!bought.empty() && !sold.empty()){
+        buy = bought.front();
+        sell = sold.front();
+
+        if(buy.count < sell.count){
+            gain += buy.count * (sell.price - buy.price);
+            bought.pop();
+
+            sold.front().count -= buy.count;
+        }else{
+            gain += sell.count * (sell.price - buy.price);
+            sold.pop();
+            if(buy.count == sell.count){
+                bought.pop();
+            }else{
+                bought.front().count -= sell.count;
+            }
+        }
+    }
+
+    std::cout<<gain;
+
+
     
     return 0;
 }
