b0ad4e40194972ea5d830a7a4e1682e4ef3fa1d2
Zain-03 <mzain.akhtar3@gmail.com>
2023-05-09 10:44:55 +0500

    updatted

--- Assignment/BST.h
+++ Assignment/BST.h
@@ -365,7 +365,29 @@ bool BST<T>::isBST(Node<T> *root)
 
     return true;
 }
-
+int isBST(struct node* node)
+{
+    if (node == NULL)
+        return 1;
+ 
+    /* false if the max of the left is > than us */
+    if (node->left != NULL
+        && maxValue(node->left) > node->data)
+        return 0;
+ 
+    /* false if the min of the right is <= than us */
+    if (node->right != NULL
+        && minValue(node->right) < node->data)
+        return 0;
+ 
+    /* false if, recursively, the left or right is not a BST
+     */
+    if (!isBST(node->left) || !isBST(node->right))
+        return 0;
+ 
+    /* passing all that, it's a BST */
+    return 1;
+}
 template <class T>
 void BST<T>::levelOrderInsertion(Node<T> *root, T data)
 {
