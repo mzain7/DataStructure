86a85c564e45174f6cd778a31e0508948e273381
Zain-03 <mzain.akhtar3@gmail.com>
2023-05-25 22:32:21 +0500

    heap linkedlist

--- Heap/Main.cpp
+++ Heap/Main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
-#include "Heap.h"
+#include "HeapTree.h"
 int main(int argc, char const *argv[])
 {
-    Heap<int> heap;
+    HeapTree heap;
     heap.insert(3);
     heap.insert(5);
     heap.insert(8);
     heap.insert(1);
-    heap.print();
+    heap.insert(2);
+    heap.insert(4);
+    heap.insert(6);
+    heap.insert(7);
+    heap.insert(9);
+    heap.insert(10);
+    heap.insert(11);
+    heap.print(heap.root);
+    heap.deletion();
+    heap.print(heap.root);
+    heap.deletion();
+    heap.print(heap.root);
+    heap.deletion();
+    heap.print(heap.root);
+    heap.deletion();
+    heap.print(heap.root);
+    heap.deletion();
 
-    heap.extractMax();
-    heap.print();
-    heap.extractMax();
-    heap.print();
-    heap.extractMax();
-    heap.print();
+
+
     return 0;
 }
